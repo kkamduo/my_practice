86d4dd0c7cce45a7e0a7900b4e2f789ac495ee70
kkamduo <wotjr05@gmail.com>
2023-07-26 12:07:31 +0900

    MLP Developer On DAY 1

--- main.py
+++ main.py
@@ -7,9 +7,18 @@
 # import pandas as pd
 import numpy as np
 
+# input - hidden Layer
+
+w1 = np.random.randn(2,2)
+b1 = np.random.randn(2,2)
+
+# hidden - ouput layer
+
+w2 = np.random.randn(1,2)
+b2 = np.random.randn(1)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a = np.array(range(0, 6))
-    # omg
-    # asdf  asd
+
     print(a)

4b09db6c310fbeb3b7bb1752b030d0cf8effd396
kkamduo <wotjr05@gmail.com>
2023-07-26 10:37:35 +0900

    Hi

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import pandas as pd
+import numpy as np
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a = np.array(range(0, 6))
+    print(a)

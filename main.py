002446a1871785605ee172f88081915f93bd951b
kkamduo <wotjr05@gmail.com>
2023-07-26 11:13:03 +0900

    my test3

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-import pandas as pd
+# import pandas as pd
 import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a = np.array(range(0, 6))
-    #omg
-    #asdf
+    # omg
+    # asdf
     print(a)

60794dbb857af48d9777a53394b7b80a262827ab
kkamduo <wotjr05@gmail.com>
2023-07-26 12:14:39 +0900

    Follow The Blog

--- main.py
+++ main.py
@@ -7,18 +7,19 @@
 # import pandas as pd
 import numpy as np
 
-# input - hidden Layer
+# train data (XOR Problem)
+x = np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([0,1,1,0])
 
+# input - hidden Layer
 w1 = np.random.randn(2,2)
 b1 = np.random.randn(2,2)
 
 # hidden - ouput layer
-
 w2 = np.random.randn(1,2)
 b2 = np.random.randn(1)
 
 # Epoch
-
 ep = 20000
 
 # Learning Rate
@@ -27,6 +28,22 @@ mse = []
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    a = np.array(range(0, 6))
+    for i in range(ep):
+        E = np.array([])
+        result = np.array([])
 
-    print(a)
+        for j in range(len(x)):
+            Ha = np.array([])
+
+            # Feed Foward
+            # Input - Hidden Layer
+            for k in range(len(w1)):
+                Ha = np.append(Ha, 1 / (1 + np.exp(-(np.sum(x[j] * w1[k]) + b1[0][k]))))
+                # Use Sigmoid Function
+
+            # Hidden - Output Layer
+            Hb = 1 / (1 + np.exp(-(np.sum(Ha * w2) + b2)))
+
+            # Error
+            E = np.append(E , y[j] - Hb)
+            result = np.append(result, Hb)

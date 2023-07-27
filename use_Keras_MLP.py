8bad9556ed89af05e8b938fb304db3ac8917bbe3
kkamduo <wotjr05@gmail.com>
2023-07-27 13:48:30 +0900

    Follow the Blog (Use Keras)

--- use_Keras_MLP.py
+++ use_Keras_MLP.py
@@ -1,5 +1,21 @@
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([[1,1],[1,0],[0,1],[0,0]])
+y = np.array([[0],[1],[1],[0]])
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=4, input_shape=(3,))
+    tf.keras.layers.Dense(units=2, activation='sigmoid', input_shape=(2,)), # First Hidden Layer
+    tf.keras.layers.Dense(units=1, activation='sigmoid'), # Second Hidden Layer
 ])
+
+model.compile(
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.1),
+    loss='mse'
+)
+model.summary
+
+history = model.fit(x,y, epochs=100, batch_size=1)
+plt.plot(history,histroy['loss'])
+plt.show()

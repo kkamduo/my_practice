88978581b5b4e6db5d131688317d962af0ccb3e4
kkamduo <wotjr05@gmail.com>
2023-11-30 05:56:29 +0000

    memo of abnorm_detection

---
+++ abnorm_detection.py
@@ -0,0 +1,15 @@
+'''
+Methods of Anomaly Detection
+
+1. Model-based Methods
+ - Isolation Forest : It is Tree based method, that detections anomaly data by dividing and isolating.
+ - 1-Class SVM : Define the area where data exists, and data outside the area are considered.
+
+2. Density/Distance-based Methods
+ - Gaussian Mixture Model
+ - k-Nearest Neighbours (kNN) method
+ - LOF(Local Outlier Factors) : Detections to outliter by creating majority and minority clusters through data density or distance scale
+
+3. Reconstruction-based Methods
+ - PCA(Principal Component Analysis) Method
+'''
